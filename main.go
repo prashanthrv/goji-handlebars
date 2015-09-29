766ac771bfcb40b3160ce00b61b90bf4c3500002
Prashanth Rajasekar <prashanthrajasekhar@gmail.com>
2015-09-29 18:41:25 +0530

    Lib push

--- main.go
+++ main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"flag"
 	"net/http"
+	"fmt"
 	
-	//"github.com/golang/glog"
+	"github.com/golang/glog"
 	//"github.com/gorilla/context"
 
-	"github.com/prashanthrv/goji-handlebars/controllers"
+	//"github.com/prashanthrv/goji-handlebars/controllers"
 	"github.com/prashanthrv/goji-handlebars/system"
 
 	"github.com/zenazn/goji"
-	//"github.com/zenzan/goji/graceful"
-	"github.com/zenzan/goji/web"
+	//"github.com/zenazn/goji/graceful"
+	"github.com/zenazn/goji/web"
 )
+
+func root(c web.C, w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hey")
+}
 
 func main() {
 	filename := flag.String("config","config.toml","Path to configuration file")
